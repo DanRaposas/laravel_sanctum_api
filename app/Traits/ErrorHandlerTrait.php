5ee7b7a48a47347c470e1d4b159d9c0ff302b0d6
Adonis Maniego <danraposas3@gmail.com>
2024-10-23 01:43:16 +0800

    HTTP Codes for error handling

--- app/Traits/ErrorHandlerTrait.php
+++ app/Traits/ErrorHandlerTrait.php
@@ -6,14 +6,24 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 
 trait ErrorHandlerTrait {
+    // Traits
     use HttpResponsesTrait;
 
-    // Method for returning responses after throwing an exception or error
+    /**
+     * Method for returning responses after throwing an exception or error
+    */
     protected function handleThrowable(\Throwable $error): JsonResponse
     {
-        $errorCode = !$error->getCode() ? 500 : $error->getCode();
         $errorMessage = $error->getMessage() ?? 'Error';
 
+        // Executed if the error code is invalid or not an int
+        if($error->getCode() < 400 || !is_int($error->getCode())) {
+            $errorCode = 500;
+        } else {
+            $errorCode = $error->getCode();
+        }
+
+        // Executed if the thrown exception is authorization-related
         if($error instanceof AuthorizationException) {
             return $this->error([
                 'error_code' => 403,
